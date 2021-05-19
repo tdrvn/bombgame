c15daec7480b2e6f7af7b94afc101f30a7c4290e
lucaperju <lucaperju@gmail.com>
2021-05-19 12:29:12 +0300

    Added a stupid player that plays random moves (might have bugs lol)

---
+++ src/player.cpp
@@ -0,0 +1,39 @@
+#include <initializer_list>
+#include <stdio.h>
+#include <stdlib.h>
+#include "game.h"
+#include "protocol.h"
+
+int main(int argc, char** argv) {
+  char* pipeFiles[2];
+  pipeFiles[0] = argv[1];
+  pipeFiles[1] = argv[2];
+  FILE *pipes[2];
+  pipes[0] = fopen(pipeFiles[0], "rb");
+  pipes[1] = fopen(pipeFiles[1], "wb");
+
+  ServerMessage serverMessage;
+  
+  PlayerMessage myMessage;
+  
+  receiveGameState(pipes[0], &serverMessage);
+  
+  int IAm = serverMessage.currentPlayer;
+  while (serverMessage.table.gameState == PLAYING) {
+	for(int i = 0; i < serverMessage.table.players[IAm].speed; ++i)
+	{
+		if(rand()%20==0)
+			myMessage.actions[i] = MOVE_BOOM;
+		else
+			myMessage.actions[i] = rand() % 5;
+	}
+    sendMove(pipes[1], myMessage);
+    receiveGameState(pipes[0], &serverMessage);
+  }
+  
+  for (int i : {0, 1}) {
+    fclose(pipes[i]);
+  }
+  printf("Player ends gracefully!\n");
+  return 0;
+}
